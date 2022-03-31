21211968797039b25df31356e0aec37a7bf47cc5
yebin Park <yebinp1102@naver.com>
2022-03-31 15:23:13 +0900

    create register api at the backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const config = require('./config/key')
 const mongoose = require('mongoose');
@@ -11,6 +15,15 @@ mongoose.connect(config.mongoURI)
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+// 회원가입 라우트
+app.post('/api/user/register', (req, res)=>{
+  const user = new User(req.body)
+  user.save((err, userInfo)=>{
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({success: true})
+  })
 })
 
 app.listen(port, ()=>console.log(`Server ${port} successfully connected!`))
---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 6,
+    maxlength: 20
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    maxlength: 24
+  },
+  role: {
+    type: Number,
+    default: 0
+  },
+  image: String,
+  token: {
+    type: String
+  },
+  tokenExp: {
+    type: Number
+  }
+})
+
+userSchema.pre('save', function(next){
+  const user = this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err)
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err)
+        user.password = hash
+        next()
+      });
+    });
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User } 
