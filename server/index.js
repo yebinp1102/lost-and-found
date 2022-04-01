972acac9c0250f4c10352ff3867572b45d512f5a
yebin Park <yebinp1102@naver.com>
2022-04-01 10:11:56 +0900

    create login api with bcrypt

--- server/index.js
+++ server/index.js
@@ -17,8 +17,21 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// 로그인 라우트
+app.post('api/users/login', (req, res)=>{
+  User.findOne({email: req.body.email}, (err, user)=>{
+    if(!user){
+      return res.json({loginSuccess: false, message: '존재하지 않는 유저 입니다.'})
+    }
+    user.comparePassword(req.body.password, (err, isMatch)=>{
+      if(!isMatch)
+      return res.json({loginSuccess: false, message: '비밀번호가 틀렸습니다.'})
+    })
+  })
+})
+
 // 회원가입 라우트
-app.post('/api/user/register', (req, res)=>{
+app.post('/api/users/register', (req, res)=>{
   const user = new User(req.body)
   user.save((err, userInfo)=>{
     if(err) return res.json({success: false, err})
--- server/models/User.js
+++ server/models/User.js
@@ -43,8 +43,17 @@ userSchema.pre('save', function(next){
         next()
       });
     });
+  }else{
+    next()
   }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+    if(err) return cb(err)
+    cb(null, isMatch)
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
